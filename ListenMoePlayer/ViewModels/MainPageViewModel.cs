0c8aaf0a92c99f2d1bd8726ff004207c81941f54
rancor1223 <rancor1223@gmail.com>
2017-04-16 19:38:12 +0200

    Add project files.

---
+++ ListenMoePlayer/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Template10.Mvvm;
+using Template10.Services.NavigationService;
+using Windows.UI.Xaml.Navigation;
+
+namespace ListenMoePlayer.ViewModels {
+	public class MainPageViewModel : ViewModelBase {
+	}
+}
